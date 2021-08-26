91bd106f254d902cb079940aefd6ccd4b5220f93
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-08-26 02:00:56 +0200

    fix: single config files works now

--- bin/make_installer.sh
+++ bin/make_installer.sh
@@ -29,8 +29,8 @@ rh_dir="${df_path}/config"
 files=`cd $rh_dir && find . -mindepth 1 -maxdepth 1 -not -iname '*_bak' `
 for file in $files; do
     file=${file##./}
-    # echo "[ -e $HOME/$file ] || ln -s --verbose ${df_path}/config/$file ~/" >> $installsh
-    echo "[ -f  $HOME/$file ] && [ ! -h $HOME/$file ] && echo The file $HOME/$file already exists and is a regular file to backup || [ -h $HOME/$file ] || ln -s --verbose ${df_path}/config/$file ~/" >> $installsh
+    # warn if is a regular file otherwise overwrite
+    echo "[ -f  $HOME/$file ] && [ ! -h $HOME/$file ] && echo The file $HOME/$file already exists and is a regular file to backup || ln -sf --verbose ${df_path}/config/$file ~/" >> $installsh
 done
 
 ## Istruction to create links to files in single_config_files.
@@ -42,9 +42,10 @@ if [[ -d "${config_files_dir}" ]]; then
     files=`cd $config_files_dir && find . -mindepth 1 \( -type f -or -type l \) -not -iname '*_bak' `
     for file in $files; do
 	file=${file#./}
-	[ -d "$HOME/$(dirname $file)" ] || mkdir -p ~/$(dirname $file)/
-	# echo "[ -e $HOME/$file ] || ln -s --verbose ~/${df_path}/single_config_files/$file ~/$(dirname $file)/" >> $installsh
-	echo "[ -f  $HOME/$file ] && [ ! -h $HOME/$file ] && echo The file $HOME/$file already exists and is a regular file to backup || [ -h $HOME/$file ] || ln -s --verbose ~/${df_path}/single_config_files/$file ~/$(dirname $file)/" >> $installsh
+	# create the parent folders
+	echo "[" -d "$HOME/$(dirname $file)" "]" "||" mkdir -p "~/"$(dirname $file)/ >> $installsh
+        # warn if is a regular file otherwise overwrite
+	echo "[ -f  $HOME/$file ] && [ ! -h $HOME/$file ] && echo The file $HOME/$file already exists and is a regular file to backup || ln -sf --verbose ${df_path}/single_config_files/$file ~/$(dirname $file)/" >> $installsh
     done
 fi
 
